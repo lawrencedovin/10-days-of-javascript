44e0395b7d656ec15c79b9b36a355ddb0f0b9a22
lawrencedovin <lawrence@qwikwire.com>
2020-08-07 15:45:57 -0400

    Completed day 4 Count Objects challenge.

--- day-4-count-objects/lib/script.js
+++ day-4-count-objects/lib/script.js
@@ -1,9 +1,17 @@
-function reverseString(s) {
-    try {
-        console.log(s.split("").reverse().join(""));
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    let count = 0;
+    for(let object of objects){
+        if(1 <= object.x||object.y && object.x||object.y >= 100) {
+            if(object.x == object.y){
+                count++;
+            } 
+        }
     }
-    catch {
-        console.log("s.split is not a function");
-        console.log(s);
-    }
+    return count;
 }
