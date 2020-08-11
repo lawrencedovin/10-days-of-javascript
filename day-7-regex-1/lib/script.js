e9d20e32f070ffe284f3f3582369f0d234a88aff
lawrencedovin <lawrence@qwikwire.com>
2020-08-10 20:02:19 -0400

    Added day 7 Regular Expressions challenge.

---
+++ day-7-regex-1/lib/script.js
@@ -0,0 +1,19 @@
+function regexVar(str) {
+        let re = /^([aeiou])[a-z]+\1$/;
+        return re;
+
+        
+    //  ^ => first item matches:
+    // () => stores matching value captured within
+    // [aeiou] => matches any of the characters in the brackets
+    // [a-z] => matches any lowercase characters
+    // + => for 1 or more occurrences (this ensures str length > 3)
+    // \1 => matches to previously stored match. 
+        // \2 looks for matched item stored 2 instances ago 
+        // \3 looks for matched item stored 3 ago, etc
+    
+    //  $ ensures that matched item is at end of the sequence
+    // The .* means that there may be characters after 
+    // that first captured character
+    // \1$ checks that the last character matches the first captured character
+    }
