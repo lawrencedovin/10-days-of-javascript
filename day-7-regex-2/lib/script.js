2948a34771c3a58d5da6d117134bdab5dbda73d9
lawrencedovin <lawrence@qwikwire.com>
2020-08-10 20:18:11 -0400

    Added day 7 Regular Expressions challenge 2.

--- day-7-regex-2/lib/script.js
+++ day-7-regex-2/lib/script.js
@@ -1,5 +1,5 @@
 function regexVar(str) {
-        let re = /^(Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]+$/;
+        let re = /^(Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]+/;
         return re;
 
         
@@ -8,12 +8,6 @@ function regexVar(str) {
     // [aeiou] => matches any of the characters in the brackets
     // [a-z] => matches any lowercase characters
     // + => for 1 or more occurrences (this ensures str length > 3)
-    // \1 => matches to previously stored match. 
-        // \2 looks for matched item stored 2 instances ago 
-        // \3 looks for matched item stored 3 ago, etc
-    
-    //  $ ensures that matched item is at end of the sequence
-    // The .* means that there may be characters after 
-    // that first captured character
-    // \1$ checks that the last character matches the first captured character
+    // | => OR
+    // \. means period
     }
