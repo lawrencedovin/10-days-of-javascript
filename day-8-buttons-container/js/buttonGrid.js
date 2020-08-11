e442947bb5e5ad8eefa1fb03953219f8b0991fdf
lawrencedovin <lawrence@qwikwire.com>
2020-08-11 19:13:37 -0400

    Added Day 8 Buttons Container challenge.

---
+++ day-8-buttons-container/js/buttonGrid.js
@@ -0,0 +1,53 @@
+let btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+for(let i = 1; i <= 9; i++){
+    switch(i){
+        case 1:
+            btn1 = document.querySelector("#btn" + i.toString());
+            btn1.innerHTML = i.toString();
+            break;
+        case 2:
+            btn2 = document.querySelector("#btn" + i.toString());
+            btn2.innerHTML = i.toString();
+            break;
+        case 3:
+            btn3 = document.querySelector("#btn" + i.toString());
+            btn3.innerHTML = i.toString();
+            break;
+        case 4:
+            btn4 = document.querySelector("#btn" + i.toString());
+            btn4.innerHTML = i.toString();
+            break;
+        case 5:
+            btn5 = document.querySelector("#btn" + i.toString());
+            btn5.innerHTML = i.toString();
+            break;
+        case 6:
+            btn6 = document.querySelector("#btn" + i.toString());
+            btn6.innerHTML = i.toString();
+            break;
+        case 7:
+            btn7 = document.querySelector("#btn" + i.toString());
+            btn7.innerHTML = i.toString();
+            break;
+        case 8:
+            btn8 = document.querySelector("#btn" + i.toString());
+            btn8.innerHTML = i.toString();
+            break;
+        case 9:
+            btn9 = document.querySelector("#btn" + i.toString());
+            btn9.innerHTML = i.toString();
+            break;
+    }
+}
+
+btn5.addEventListener('click', () => {
+    let previousBtn1Value=btn1.innerHTML; 
+    btn1.innerHTML=btn4.innerHTML; 
+    btn4.innerHTML=btn7.innerHTML; 
+    btn7.innerHTML=btn8.innerHTML; 
+    btn8.innerHTML=btn9.innerHTML; 
+    btn9.innerHTML=btn6.innerHTML; 
+    btn6.innerHTML=btn3.innerHTML; 
+    btn3.innerHTML=btn2.innerHTML;
+    btn2.innerHTML=previousBtn1Value; 
+});
